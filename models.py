db57a414f364fd28b3bd6429c2e7944bcba67240
devZamoraUSA <marcoa.sedeno@zamorausa.com>
2024-10-10 15:54:28 -0400

    update models recive on form user

--- models.py
+++ models.py
@@ -1,13 +1,23 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, validator
 from typing import Optional
-from datetime import date
+from datetime import date, datetime
 
 class FormData(BaseModel):
     name: str = Field(..., min_length=1)
     email: EmailStr
-    birthday_date: str
+    birthday_date: str  # Puede ser 'date' o 'str'
     options: str = Field(..., min_length=1)
     number_of_guests: int = Field(..., gt=0)
     special_notes: Optional[str]
+    submission_datetime: datetime
     success_url: str
     cancel_url: str
+
+    @validator('submission_datetime')
+    def validate_submission_datetime(cls, value):
+        if isinstance(value, str):
+            try:
+                return datetime.fromisoformat(value)
+            except ValueError:
+                raise ValueError('submission_datetime must be in ISO format')
+        return value
