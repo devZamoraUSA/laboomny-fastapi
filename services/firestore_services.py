58572f8f738bab2210d531539f8946343e65d4f0
devZamoraUSA <marcoa.sedeno@zamorausa.com>
2024-10-10 15:55:09 -0400

    update logic for firebase

--- services/firestore_services.py
+++ services/firestore_services.py
@@ -6,8 +6,13 @@ class FirestoreService:
         self.repository = repository
 
     def save_form_data(self, data: FormData):
-        # Lógica adicional si es necesaria
         return self.repository.save(data)
+
+    def update_payment_status(self, document_id: str, paid: bool):
+        self.repository.update_payment_status(document_id, paid)
+
+    def get_form_data(self, document_id: str):
+        return self.repository.get_form_data(document_id)
 
 # Dependency Injection
 def get_firestore_service():
